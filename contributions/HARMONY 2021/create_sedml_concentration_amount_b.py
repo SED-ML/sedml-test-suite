26766f40ae38dc06fecfd56b2adab629e05125c0
Lucian Smith <lpsmith@uw.edu>
2021-05-21 15:30:20 -0700

    Add tests for jacobian, concentration, and amount.

---
+++ contributions/HARMONY 2021/create_sedml_concentration_amount_b.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 10 14:58:01 2021
+
+@author: Lucian
+"""
+
+import tellurium as te
+import phrasedml
+import libsedml
+import libsbml
+import sys
+
+r = te.loada("""
+   model case_01
+       substanceonly species S1 in C
+       substanceonly species S2 in C
+       S1 = 10
+       S2 = 5
+       C = 3
+       S1 -> S2; S1*k
+       k = 0.3
+   end
+   """)
+
+SBML = r.getSBML()
+
+p_str = """
+    model0 = model "case_03.xml"
+    sim0 = simulate uniform(0, 10, 10)
+    task0 = run sim0 on model0
+    plot "UniformTimecourse default" time vs S1, S2
+    plot "UniformTimecourse amounts" time vs S1, S2
+    plot "UniformTimecourse concentration" time vs S1, S2
+"""
+te.saveToFile("case_03.xml", SBML)
+
+# phrasedml.setReferencedSBML("case_01.xml", SBML)
+sed = phrasedml.convertString(p_str)
+if sed is None:
+    print(phrasedml.getLastError())
+    sys.exit()
+
+sedml = libsedml.readSedMLFromString(sed)
+sedml.setVersion(4)
+
+datagen = sedml.getDataGenerator(1).clone()
+var = datagen.getVariable(0)
+var.setSymbol("urn:sedml:symbol:amount")
+datagen.setId("S1_amt")
+sedml.addDataGenerator(datagen)
+
+datagen = sedml.getDataGenerator(2).clone()
+var = datagen.getVariable(0)
+var.setSymbol("urn:sedml:symbol:amount")
+datagen.setId("S2_amt")
+sedml.addDataGenerator(datagen)
+
+plot = sedml.getOutput(1)
+curve = plot.getCurve(0)
+curve.setYDataReference("S1_amt")
+curve = plot.getCurve(1)
+curve.setYDataReference("S2_amt")
+
+
+datagen = sedml.getDataGenerator(1).clone()
+var = datagen.getVariable(0)
+var.setSymbol("urn:sedml:symbol:concentration")
+datagen.setId("S1_conc")
+sedml.addDataGenerator(datagen)
+
+datagen = sedml.getDataGenerator(2).clone()
+var = datagen.getVariable(0)
+var.setSymbol("urn:sedml:symbol:concentration")
+datagen.setId("S2_conc")
+sedml.addDataGenerator(datagen)
+
+plot = sedml.getOutput(2)
+curve = plot.getCurve(0)
+curve.setYDataReference("S1_conc")
+curve = plot.getCurve(1)
+curve.setYDataReference("S2_conc")
+
+
+
+sedstr = libsedml.writeSedMLToString(sedml)
+print(sedstr)
+
+import os
+sedfile = os.path.basename(__file__)
+sedfile = sedfile.replace(".py", ".sedml")
+sedfile = sedfile.replace("create_sedml_", "")
+
+te.saveToFile(sedfile, sedstr)
+
+te.executeSEDML(sedstr, saveOutputs=True)
+
---
+++ contributions/HARMONY 2021/create_sedml_jacobian.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 10 14:58:01 2021
+
+@author: Lucian
+"""
+
+import tellurium as te
+import phrasedml
+import libsedml
+import libsbml
+import sys
+
+r = te.loada("""
+   model case_01
+       species S1=10, S2=5
+       S1 -> S2; S1*k
+       k = 0.3
+   end
+   """)
+
+SBML = r.getSBML()
+
+p_str = """
+    model0 = model "case_01.xml"
+    sim0 = simulate uniform(0, 10, 10)
+    task0 = run sim0 on model0
+    report task0.time, task0.S1
+"""
+te.saveToFile("case_01.xml", SBML)
+
+# phrasedml.setReferencedSBML("case_01.xml", SBML)
+sed = phrasedml.convertString(p_str)
+if sed is None:
+    print(phrasedml.getLastError())
+    sys.exit()
+
+sedml = libsedml.readSedMLFromString(sed)
+sedml.setVersion(4)
+
+datagen = sedml.createDataGenerator()
+datagen.setId("jacobian")
+var = datagen.createVariable()
+var.setId("j")
+var.setModelReference("model0")
+var.setTaskReference("task0")
+var.setSymbol("urn:sedml:analysis:jacobian:full")
+astn = libsbml.parseL3Formula("j");
+datagen.setMath(astn)
+
+report = sedml.getOutput(0).clone()
+report.removeDataSet(1)
+report.setId("report_2")
+ds = report.getDataSet(0)
+ds.setId("Jacobian_report")
+ds.setLabel("Jacobian")
+ds.setDataReference("jacobian")
+ret = sedml.addOutput(report)
+
+
+
+sedstr = libsedml.writeSedMLToString(sedml)
+print(sedstr)
+
+import os
+sedfile = os.path.basename(__file__)
+sedfile = sedfile.replace(".py", ".sedml")
+sedfile = sedfile.replace("create_sedml_", "")
+
+te.saveToFile(sedfile, sedstr)
+
+te.executeSEDML(sedstr, saveOutputs=True)
+
